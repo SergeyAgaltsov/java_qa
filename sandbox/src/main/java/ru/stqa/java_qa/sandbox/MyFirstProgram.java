c58d38d93d3343282557197c55bedcbb2c520764
SergeyAgaltsov <79686212010@ya.ru>
2018-11-19 21:05:43 +0300

    Добавлены новые функции

--- sandbox/src/main/java/ru/stqa/java_qa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java_qa/sandbox/MyFirstProgram.java
@@ -3,7 +3,28 @@ package ru.stqa.java_qa.sandbox;
 public class MyFirstProgram {
 
    public static void main(String[] args) {
-System.out.println("Hello,world!");
+      hello ("user");
+      hello("world");
+      hello("Sergey");
+
+      double len = 5;
+      System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+      double a = 6;
+      double b = 8;
+      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+   }
+
+   public static void hello(String somebody){
+      System.out.println("Hello, " + somebody + "!" );
+   }
+
+   public static double area (double len){
+      return len * len;
+   }
+
+   public static double area (double a, double b){
+      return a * b;
    }
 }
 

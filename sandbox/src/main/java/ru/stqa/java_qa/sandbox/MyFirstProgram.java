7f3530355af0cc4ece8bb32487dd758f2887deef
SergeyAgaltsov <79686212010@ya.ru>
2018-11-19 21:33:40 +0300

    Функции вычисления площадей перенесены в классы

--- sandbox/src/main/java/ru/stqa/java_qa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/java_qa/sandbox/MyFirstProgram.java
@@ -7,24 +7,15 @@ public class MyFirstProgram {
       hello("world");
       hello("Sergey");
 
-      double len = 5;
-      System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+      Square s = new Square(5);
+      System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-      double a = 6;
-      double b = 8;
-      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+      Rectangle r = new Rectangle(6,8);
+      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
    }
 
    public static void hello(String somebody){
       System.out.println("Hello, " + somebody + "!" );
-   }
-
-   public static double area (double len){
-      return len * len;
-   }
-
-   public static double area (double a, double b){
-      return a * b;
    }
 }
 

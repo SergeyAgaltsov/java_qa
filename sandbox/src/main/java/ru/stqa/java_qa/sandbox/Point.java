83b4dc7641e77157e34de6dc5adb3cab13d8d525
SergeyAgaltsov <79686212010@ya.ru>
2018-11-21 18:13:31 +0300

    Revert "Домашнее задание 2 (финальная версия)"
    This reverts commit 2eae2af915feba2e2e1af9ac2fe578ff56399d08.

--- sandbox/src/main/java/ru/stqa/java_qa/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/java_qa/sandbox/Point.java
@@ -2,23 +2,19 @@ package ru.stqa.java_qa.sandbox;
 
 public class Point {
 
-    public double x1;
-    public double y1;
-    public double x2;
-    public double y2;
+    public double x;
+    public double y;
 
 
-    public Point(double x1, double y1, double x2, double y2 ){
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+    public Point(double xCoord, double yCoord){
+        this.x = xCoord;
+        this.y = yCoord;
     }
 
     public double getDistance()
     {
-        double dx = this.x2 - this.x1;
-        double dy = this.y2 - this.y1;
+        double dx = this.x - this.x;
+        double dy = this.y - this.y;
         return Math.sqrt(dx * dx + dy * dy);
     }
 }

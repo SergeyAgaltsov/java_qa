2eae2af915feba2e2e1af9ac2fe578ff56399d08
SergeyAgaltsov <79686212010@ya.ru>
2018-11-21 11:26:35 +0300

    Домашнее задание 2 (финальная версия)

--- sandbox/src/main/java/ru/stqa/java_qa/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/java_qa/sandbox/Distance.java
@@ -4,11 +4,7 @@ public class Distance {
 
     public static void main(String[] args) {
 
-        Point p1;
-        p1 = new Point(2,2);
-        Point p2;
-        p2 = new Point(5,6);
-        Point p = new Point();
+        Point p = new Point(2,2,2,1);
         System.out.println("Растояние между точками " + p.getDistance());
     }
 }

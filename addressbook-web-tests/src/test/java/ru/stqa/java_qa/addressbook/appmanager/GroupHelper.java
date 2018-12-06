46104af8d61753317e83263570c0b1eeb8c6a6fc
SergeyAgaltsov <79686212010@ya.ru>
2018-12-07 00:09:02 +0300

    Правки мелочей

--- addressbook-web-tests/src/test/java/ru/stqa/java_qa/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_qa/addressbook/appmanager/GroupHelper.java
@@ -2,7 +2,6 @@ package ru.stqa.java_qa.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.java_qa.addressbook.model.GroupData;
 
 public class GroupHelper extends HelperBase {
--- addressbook-web-tests/src/test/java/ru/stqa/java_qa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_qa/addressbook/tests/ContactCreationTests.java
@@ -8,7 +8,7 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() {
     app.getContactHelper().gotoContactPage();
-    app.getContactHelper().createContact(new ContactDate("Java course","Soft QA","Moscow, Lenina 44","1@qa.ru","2@qa.ru", "3@qa.tu", "Ivan","Ivanov","Ivanovich","5555555","666666","777777", "test1"), true);
+    app.getContactHelper().createContact(new ContactDate("Java course","Soft QA","Moscow, Lenina 44","1@qa.ru","2@qa.ru", "3@qa.ru", "Ivan","Ivanov","Ivanovich","5555555","666666","777777", "test1"), true);
 
   }
 

22757053d426082e897528e416941e856949df65
SergeyAgaltsov <79686212010@ya.ru>
2018-12-06 22:48:51 +0300

    Добавлен проверка наличия группы в предусловиях

--- addressbook-web-tests/src/test/java/ru/stqa/java_qa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_qa/addressbook/tests/GroupDeletionTests.java
@@ -2,12 +2,16 @@ package ru.stqa.java_qa.addressbook.tests;
 
 
 import org.testng.annotations.*;
+import ru.stqa.java_qa.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();

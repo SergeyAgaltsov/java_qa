4a8705208be70c0162f8cd1834505e1564f7a167
SergeyAgaltsov <79686212010@ya.ru>
2018-12-06 23:18:52 +0300

    Починил создание контактов

--- addressbook-web-tests/src/test/java/ru/stqa/java_qa/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_qa/addressbook/tests/GroupModificationTests.java
@@ -9,11 +9,11 @@ public class GroupModificationTests extends TestBase {
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
         if (! app.getGroupHelper().isThereGroup()) {
-            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
         }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test11","test22","test33"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test1","test2","test3"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
